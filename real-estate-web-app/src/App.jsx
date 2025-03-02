483565ddd9cb3a56c69471898b12081025b1206e
Ashu <ashutoshkhade2530@gmail.com>
2025-03-02 18:45:08 +0530

    Initialize main App component structure

--- real-estate-web-app/src/App.jsx
+++ real-estate-web-app/src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 import Testimonails from "./components/Testimonails";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/Footer";
-import Contact from "./components/Contact";
 
 const App = () => {
   return (
